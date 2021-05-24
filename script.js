c6870107cd7dbc60b1a30f31b3ae10f207605c30
Ryan Anderson <ryan0425@msn.com>
2021-05-23 23:33:01 -0700

    cleaned up files and adjusted scripts src in html

--- script.js
+++ script.js
@@ -6,126 +6,117 @@ var forecast = document.querySelector("#forecast");
 var historyContainer = document.querySelector("#history-container");
 
 function renderHistory() {
-  searchHistory.innerHTML = "";
-  for (var i = searchHistory.length - 1; i >= 0; i--) {
-    var button = document.createElement("button");
-    bnt.setAttribute("class", "btn-history list-group-item-action");
-    btn.setAttribute("type", "button");
-    btn.setAttribute("aria-controls", "today forecast");
-    btn.setAttribute("data-search", searchHistory[i]);
-    btn.textContent = searchHistory[i];
-    searchHistory.append(btn);
+    searchHistory.innerHTML = "";
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+        var button = document.createElement("button");
+        bnt.setAttribute("class", "btn-history list-group-item-action");
+        btn.setAttribute("type", "button");
+        btn.setAttribute("aria-controls", "today forecast");
+        btn.setAttribute("data-search", searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistory.append(btn);
 
-  }
+    }
 }
 
 
-
-
-// var languageButtonsEl = document.querySelector('#language-buttons');
-// var nameInputEl = document.querySelector('#username');
-// var repoContainerEl = document.querySelector('#repos-container');
-// var repoSearchTerm = document.querySelector('#repo-search-term');
-// var temp = document.querySelector('temp');
-// var desc = document.querySelector('desc');
-// var name = document.querySelector('name');
-
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  var username = nameInputEl.value.trim();
-
-  if (username) {
-    getUserRepos(username);
-
-    repoContainerEl.textContent = '';
-    nameInputEl.value = '';
-  } else {
-    alert('Please enter another city');
-  }
-};
-
-var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
-
-  if (language) {
-    getFeaturedRepos(language);
-
-    repoContainerEl.textContent = '';
-  }
-};
-
-var getUserRepos = function (user) {
-  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
-
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayRepos(data, user);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('wrong city name');
-    });
-};
-
-var getFeaturedRepos = function (language) {
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('a');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
-
-
 initSearchHistory();
-userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+searchForm.addEventListener("submit", handleSearchFormSubmit);
+searchHistoryContainer.addEventListener("click", handleSearchHistoryClick);
+
+
+
+
+// window.addEventListener("load", function () {
+//     var temp = document.querySelector('temp');
+//     var desc = document.querySelector('desc');
+//     var name = document.querySelector('name');
+//     var search = document.querySelector('#search');
+//     var city = document.getElementById('city');
+//     console.log(city.value);
+
+//     function renderweather(weather) {
+//         console.log(weather);
+
+//         var resultsContainer = document.querySelector("#weather-results");
+//         var city = document.createElement("h2");
+//         city.textContent = weather.name;
+//         resultsContainer.append(city);
+//         var temp = document.createElement("p");
+//         temp.textContent = "Temp: " + weather.main.temp + "°F";
+//         resultsContainer.append(temp);
+//         var humidity = document.createElement("p");
+//         humidity.textContent = "Humidity: " + weather.main.humidity + "%";
+//         resultsContainer.append(humidity);
+//         var wind = document.createElement("p");
+//         wind.textContent = "Wind: " + weather.wind.speed + "mph,";
+//         resultsContainer.append(wind);
+
+//         weather.weather[0].name = weather.name;
+//         weather.weather[0].temph = 88;
+//         weather.weather[0].templ = 53;
+//         var weatherDetails = weather.weather[0]
+//         weather.weather[1] = weather.weather[0]
+//         weather.weather[2] = weather.weather[0]
+//         weather.weather[3] = weather.weather[0]
+//         weather.weather[4] = weather.weather[0]
+//         console.log(weather);
+//         if (weatherDetails && weatherDetails.description) {
+//             var description = document.createElement("p");
+//             description.textContent = weatherDetails.description;
+//             resultsContainer.append(description);
+//         }
+//         var fiveDayContainer = document.querySelector("#FiveDayForecast");
+//         console.log(weather.weather);
+//         for (x = 0; x < weather.weather.length; x++) {
+
+//             var card = document.createElement("div");
+//             card.classList.add("card");
+
+
+//             var city = document.createElement("h2");
+//             city.textContent = weather.weather[x].name;
+//             card.append(city);
+//             var temp = document.createElement("p");
+//             temp.textContent = "HighTemp: " + weather.weather[x].temph + "°F";
+//             card.append(temp);
+//             var templ = document.createElement("p");
+//             templ.textContent = "LowTemp: " + weather.weather[x].templ + "°F";
+//             card.append(templ);
+//             // var humidity = document.createElement("p");
+//             // humidity.textContent = "Humidity: " + weather.main.humidity + "%";
+//             // card.append(humidity);
+//             // var wind = document.createElement("p");
+//             // wind.textContent = "Wind: " + weather.wind.speed + "mph,";
+//             // card.append(wind);
+//             fiveDayContainer.append(card);
+//         }
+//     }
+
+
+//     search.addEventListener('click',
+//         function (event) {
+//             event.preventDefault();
+//             var query = city.value;
+//             console.log(city.value);
+//             var url = "http://api.openweathermap.org/data/2.5/weather?q=" +
+//                 query +
+//                 "&units=imperial&appid=d91f911bcf2c0f925fb6535547a5ddc9";
+
+//             fetch(url)
+//                 .then(response => response.json())
+//                 .then(data => renderweather(data));
+//         }
+//     );
+
+// })
+
+
+
+
+
+
+
+
+
+
